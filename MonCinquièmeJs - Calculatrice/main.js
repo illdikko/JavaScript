d49c7f8a84560ccd54414f5169a71bc40a42f4da
Coline Ducourtieux <coline.ducourtieux@gmail.com>
2022-09-05 15:30:05 +0200

    added exo calculatrice

---
+++ MonCinquièmeJs - Calculatrice/main.js
@@ -0,0 +1,63 @@
+let input1;
+let input2;
+let operator;
+let decimalPressed=false;
+let equalPressed=false;
+const html_show = document.getElementById("show");
+html_show.value = 0;
+
+
+const pressNumber = function(nb)
+{
+    if (equalPressed){
+        html_show.value = 0
+        equalPressed = false;
+    }
+    if(nb==0 && html_show.value === "0") nb="";
+    if(nb!=0 && nb!='.' && html_show.value ==="0") html_show.value="";
+    html_show.value += nb;
+}
+
+const pressDecimal = function ()
+{
+    if (!decimalPressed){
+        pressNumber(".");
+        decimalPressed = true;
+    }
+    else console.warn (`Decimal already pressed`)
+}
+
+const pressOperator = function (operateur)
+{
+    input1 = parseFloat(html_show.value);
+    operator = operateur;
+    html_show.value="0";
+    decimalPressed = false;
+}
+
+const pressEqual = function () 
+{
+    if (!input1 && input1 != 0) throw new Error ("Pas de valeur");
+    if (!operator) throw new Error ("Pas d'opérateur choisi");
+    input2 = parseFloat(html_show.value);
+
+    switch (operator) {
+        case "/":
+            html_show.value = input1 / input2
+            break;
+        case "*":
+            html_show.value = input1 * input2
+
+            break;
+        case "+":
+            html_show.value = input1 + input2
+
+            break;
+        case "-":
+            html_show.value = input1 - input2
+
+            break;
+    }
+    equalPressed = true;
+    decimalPressed = false;
+}
--- MonPremierJs/Scripts/main.js
+++ MonPremierJs/Scripts/main.js
@@ -1,1 +1,46 @@
-alert(`Bienvenue dans mon premier cours de JavaScript`);
+// alert(`Bienvenue dans mon premier cours de JavaScript`);
+
+// let prenom = prompt(`Quel est votre prénom?`);
+// let animal = confirm(`Êtes vous une personne à chat?`)
+
+// if(animal){alert(`Yaaay bienvenue au club!`)}
+// else{alert(`C'est si banal...`)}
+
+const html_prenom = document.getElementById("prenom");
+const html_animal_chat = document.getElementById("animalChat");
+const html_animal_chien = document.getElementById("animalChien");
+const html_animal_autre = document.getElementById("animalAutre");
+
+const saveInfo = function ()
+// la constante saveInfo est égale à une fonction non nommée.
+{
+    if (!html_prenom.value) throw new Error(`Manque information : prénom`);
+    if (!html_animal_chat.checked && !html_animal_chien.checked && !html_animal_autre.checked) throw new Error(`Manque information : animal`);
+    let prenom = html_prenom.value;
+    let animal;
+    if (html_animal_chat.checked) animal = html_animal_chat.value;
+    else if (html_animal_chien.checked) animal = html_animal_chien.value;
+    else animal = html_animal_autre.value;
+    alert(getInfo(prenom, animal))
+}
+
+const getInfo = function (prenom, animal) {
+    if (typeof (prenom) != 'string') throw new TypeError(`Prenom is not a string`);
+    if (typeof (animal) != 'string') throw new TypeError('Animal is not a string')
+    let message = `Bonjour ${prenom} `
+    switch (animal) {
+        case "Chat":
+
+            message += " , ton animal préféré est le chat!"
+            break;
+        case "Chien":
+
+            message += " , ton animal préféré est le chien!"
+            break;
+        case "Autre":
+
+            message += " , ton animal préféré n'est ni le chien, ni le chat!"
+            break;
+    }
+    return message
+}
