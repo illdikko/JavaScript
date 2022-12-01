44ffaac467bb01db2138e9a09379c914a4be23b0
« Coline <c.ducourtieux@interface3.be>
2022-12-01 21:46:12 +0100

    révisions + cheatSheet

---
+++ CheatSheetJSµ.js
@@ -0,0 +1,11 @@
+//vérifier qu'un bouton est clické ou pas
+//cf exercice Fonction+DOM Calculatrice.
+
+let buttonPressed = false;
+
+const pressButton = function () {
+    if(!buttonPressed){
+        //code à éffectué à l'appui du bouton
+        buttonPressed = true;
+    }
+}
